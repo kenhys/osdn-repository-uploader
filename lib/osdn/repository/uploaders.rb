36291aa27ef0791149469869033736c89bf008ef
HAYASHI Kentaro <kenhys@gmail.com>
2016-03-31 00:10:46 +0900

    Initial commit

---
+++ lib/osdn/repository/uploaders.rb
@@ -0,0 +1,9 @@
+require "osdn/repository/uploaders/version"
+
+module Osdn
+  module Repository
+    module Uploaders
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/osdn/repository/uploaders/version.rb
@@ -0,0 +1,7 @@
+module Osdn
+  module Repository
+    module Uploaders
+      VERSION = "0.1.0"
+    end
+  end
+end
